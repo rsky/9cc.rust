2aa61e0de9130a0166b615f6dccd0643de29e891
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2019-05-12 00:10:25 +0900

    take argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::io::Write;
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        writeln!(std::io::stderr(), "Invalid number of arguments.").unwrap();
+        std::process::exit(1);
+    }
+
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, 1");
+    println!("  mov rax, {}", args[1]);
     println!("  ret");
 }
